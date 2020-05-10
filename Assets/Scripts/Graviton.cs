86d24fbedfba6cafc2457c59562cf2271bf04335
David Nuño <davidnunogarcia@gmail.com>
2020-05-10 19:41:21 +0200

    ADD graviton base logic

---
+++ Assets/Scripts/Graviton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graviton : MonoBehaviour
+{
+
+    List<Graviton> gravitons = new List<Graviton>();
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        Graviton[] tmp = FindObjectsOfType<Graviton>();
+        foreach(Graviton graviton in tmp)
+        {
+            if(graviton != this)
+            {
+                gravitons.Add(graviton);
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        foreach(Graviton graviton in gravitons)
+        {
+            Attract(graviton);
+        }
+    }
+
+    private void Attract(Graviton graviton)
+    {
+       
+    }
+}
