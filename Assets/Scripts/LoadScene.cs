c62759b8d0549aeeba8f59d20fbf78624ea895d2
David Nuño <davidnunogarcia@gmail.com>
2020-05-11 12:21:56 +0200

    ADD loadscene script

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(changeScene);
+    }
+    
+    private void changeScene()
+    {
+        Debug.Log("Loading game scene");
+        SceneManager.LoadScene("GameScene");
+    }
+}
