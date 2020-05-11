b255af56e8fb168a66e16dba80c299e0bf4d9f2d
David Nuño <davidnunogarcia@gmail.com>
2020-05-11 12:21:25 +0200

    UPD add return button

---
+++ Assets/Scripts/ReturnToMainScene.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ReturnToMainScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(ReturnToMainMenu);
+    }
+
+    private void ReturnToMainMenu()
+    {
+        Debug.Log("Loading main menu");
+        SceneManager.LoadScene("Menu");
+    }
+
+}
