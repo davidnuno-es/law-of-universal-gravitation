005fbeeec0f81ce180ad002f2599d4f94ecdcaf0
David Nuño <davidnunogarcia@gmail.com>
2020-05-11 13:14:46 +0200

    ADD spawn slider

---
+++ Assets/Scripts/SpawnRadiusSlider.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SpawnRadiusSlider : MonoBehaviour
+{
+    public Text txt;
+    Slider slider;
+
+    float value = 0;
+
+    private void Start()
+    {
+        slider = GetComponent<Slider>();
+        value = slider.value;
+        txt.text = value + "";
+        SceneInfo.spawnRadius = value;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (value != slider.value)
+        {
+            value = slider.value;
+            txt.text = value + " m";
+            SceneInfo.spawnRadius = value;
+        }
+    }
+}
