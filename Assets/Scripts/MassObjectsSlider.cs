c7db035d11a2f33bfd1848f78d3376317742ad3e
David Nuño <davidnunogarcia@gmail.com>
2020-05-11 13:09:58 +0200

    ADD objects mass slider

---
+++ Assets/Scripts/MassObjectsSlider.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MassObjectsSlider : MonoBehaviour
+{
+    public Text txt;
+    Slider slider;
+
+    float value = 0;
+
+    private void Start()
+    {
+        slider = GetComponent<Slider>();
+        value = slider.value;
+        txt.text = value + "";
+        SceneInfo.commonMass = (int)value;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (value != slider.value)
+        {
+            value = slider.value;
+            txt.text = value + " Kg";
+            SceneInfo.commonMass = (int)value;
+        }
+    }
+}
