0eb1279dbde376b5527c17ed0b39c526566ace0c
David Nuño <davidnunogarcia@gmail.com>
2020-05-11 13:02:54 +0200

    ADD objectscount slider

---
+++ Assets/Scripts/CountObjectsSlider.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CountObjectsSlider : MonoBehaviour
+{
+
+    public Text txt;
+    Slider slider;
+
+    float value = 0;
+
+    private void Start()
+    {
+        slider = GetComponent<Slider>();
+        value = slider.value;
+        txt.text = value + "";
+        SceneInfo.objectsCount = (int)value;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(value != slider.value)
+        {
+            value = slider.value;
+            txt.text = value + "";
+            SceneInfo.objectsCount = (int)value;
+        }
+    }
+}
