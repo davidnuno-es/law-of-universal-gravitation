0363ef46047bebe89de053f04b83485fe898cc43
David Nuño <davidnunogarcia@gmail.com>
2020-05-11 12:34:50 +0200

    ADD exit game button

---
+++ Assets/Scripts/QuitGame.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class QuitGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(Exit);
+    }
+
+    // Update is called once per frame
+    private void Exit()
+    {
+        Application.Quit();
+    }
+}
